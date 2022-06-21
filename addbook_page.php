c1ad01e41de0183f7c1f6fea3acd3a305141019a
seancroix <sean_guevarra@ymail.com>
2022-06-21 20:22:53 +0800

    Added receiver when user or admin adds a book

---
+++ addbook_page.php
@@ -0,0 +1,11 @@
+<?php
+include("data_class.php");
+
+$bookname=$_POST['bookname'];
+$bookdetail=$_POST['bookdetail'];
+$bookauthor=$_POST['bookauthor'];
+$bookpublish=$_POST['bookpublish'];
+$branch=$_POST['branch'];
+$bookprice=$_POST['bookprice'];
+$bookquantity=$_POST['bookquantity'];
+
