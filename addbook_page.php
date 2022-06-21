06b154a6048d1529cfd7b84a738e9ce1121a1940
seancroix <sean_guevarra@ymail.com>
2022-06-21 21:15:30 +0800

    Set connection to the folder "uploads" & when user inputs

--- addbook_page.php
+++ addbook_page.php
@@ -9,3 +9,14 @@ $branch=$_POST['branch'];
 $bookprice=$_POST['bookprice'];
 $bookquantity=$_POST['bookquantity'];
 
+    if (move_uploaded_file($_FILES["bookphoto"]["tmp_name"],"uploads/" . $_FILES["bookphoto"]["name"])){
+        echo "Book Updated!";
+        $bookpic=$_FILES["bookphoto"]["name"];
+
+        $obj=new data();
+        $obj->setconnection();
+        $obj->addbook($bookpic,$bookname,$bookdetail,$bookauthor,$bookpublish,$branch,$bookprice,$bookquantity);
+    }
+    else{
+        echo "File cannot be uploaded"; 
+    }
