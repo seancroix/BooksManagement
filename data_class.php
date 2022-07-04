b2790f14880868e793c9b5c240b68b8d556f48fb
seancroix <sean_guevarra@ymail.com>
2022-07-04 18:37:25 +0800

    returning book conditions and response

--- data_class.php
+++ data_class.php
@@ -43,6 +43,95 @@ elseif($result <= 0){
 }
 }
 
+function returnbook($id){
+    $fine="";
+    $bookava="";
+    $issuebook="";
+    $bookrentel="";
+
+    $q="SELECT * FROM issuebook where id='$id'";
+    $recordSet=$this->connection->query($q);
+
+    foreach($recordSet->fetchAll() as $row) {
+        $userid=$row['userid'];
+        $issuebook=$row['issuebook'];
+        $fine=$row['fine'];
+
+    }
+    if($fine==0){
+
+    $q="SELECT * FROM book ";
+    $recordSet=$this->connection->query($q);
+    
+  
+
+    foreach($recordSet->fetchAll() as $row) {
+        $bookava=$row['bookava']+1;
+        $bookrentel=$row['bookrent']-1;
+    }
+    $q="UPDATE book SET bookava='$bookava', bookrent='$bookrentel' where bookname='$issuebook'";
+    $this->connection->exec($q);
+
+    $q="DELETE from issuebook where id=$id and issuebook='$issuebook' and fine='0' ";
+    if($this->connection->exec($q)){
+
+        header("Location:otheruser_dashboard.php?userlogid=$userid");
+     }
+    //  else{
+    //     header("Location:otheruser_dashboard.php?msg=fail");
+    //  }
+    }
+    // if($fine!=0){
+    //     header("Location:otheruser_dashboard.php?userlogid=$userid&msg=fine");
+    // }
+   
+
+}
+
+function getissuebook($userloginid) {
+
+    $newfine="";
+    $issuereturn="";
+
+    $q="SELECT * FROM issuebook where userid='$userloginid'";
+    $recordSetss=$this->connection->query($q);
+
+
+    foreach($recordSetss->fetchAll() as $row) {
+        $issuereturn=$row['issuereturn'];
+        $fine=$row['fine'];
+        $newfine= $fine;
+
+        
+            //  $newbookrent=$row['bookrent']+1;
+    }
+
+
+    $getdate= date("d/m/Y");
+    if($issuereturn<$getdate){
+        $q="UPDATE issuebook SET fine='$newfine' where userid='$userloginid'";
+
+        if($this->connection->exec($q)) {
+            $q="SELECT * FROM issuebook where userid='$userloginid' ";
+            $data=$this->connection->query($q);
+            return $data;
+        }
+        else{
+            $q="SELECT * FROM issuebook where userid='$userloginid' ";
+            $data=$this->connection->query($q);
+            return $data;  
+        }
+
+    }
+    else{
+        $q="SELECT * FROM issuebook where userid='$userloginid'";
+        $data=$this->connection->query($q);
+        return $data;
+
+    }
+
+}
+
 function addnewuser($name,$password,$email,$type){
     $this->name=$name;
     $this->password=$password;
@@ -69,8 +158,8 @@ function addbook($bookpic,$bookname,$bookdetail,$bookauthor,$bookpublish,$branch
     $this->bookprice=$bookprice;
     $this->bookquantity=$bookquantity;
     
-    $q="INSERT INTO book (id, bookpic, bookname, bookdetail, bookauthor, bookpublish, branch, bookprice, bookquantity, bookava, bookrent) 
-            VALUES('', '$bookpic', '$bookname', '$bookdetail', '$bookauthor', '$bookpublish', '$branch', '$bookprice', '$bookquantity', 0)";
+    $q="INSERT INTO book (bookpic, bookname, bookdetail, bookauthor, bookpublish, branch, bookprice, bookquantity, bookrent) 
+            VALUES('$bookpic', '$bookname', '$bookdetail', '$bookauthor', '$bookpublish', '$branch', '$bookprice', '$bookquantity', 0)";
 
 
     if($this->connection->exec($q)) {
@@ -90,13 +179,142 @@ function userdata(){
 }
 
 function getbookissue(){
-    $q="SELECT * FROM book where bookava !=0 ";
+    $q="SELECT * FROM book where bookquantity > 0 ";
     $data=$this->connection->query($q);
     return $data;
 }
 
-function getbookdetail(){
+function getbookdetail($id){
     $q="SELECT * FROM book where id = '$id'";
+    $data=$this->connection->query($q);
+    return $data;
+}
+
+function issuebookapprove($book,$userselect,$days,$getdate,$returnDate,$redid){
+    $this->$book= $book;
+    $this->$userselect=$userselect;
+    $this->$days=$days;
+    $this->$getdate=$getdate;
+    $this->$returnDate=$returnDate;
+
+
+    $q="SELECT * FROM book where bookname='$book'";
+    $recordSetss=$this->connection->query($q);
+
+    $q="SELECT * FROM userdata where name='$userselect'";
+    $recordSet=$this->connection->query($q);
+    $result=$recordSet->rowCount();
+
+    if ($result > 0) {
+
+        foreach($recordSet->fetchAll() as $row) {
+            $issueid=$row['id'];
+            $issuetype=$row['type'];
+
+            // header("location: admin_service_dashboard.php?logid=$logid");
+        }
+        foreach($recordSetss->fetchAll() as $row) {
+            $bookid=$row['id'];
+            $bookname=$row['bookname'];
+
+                $newbookava=$row['bookava']-1;
+                 $newbookrent=$row['bookrent']+1;
+        }
+
+    
+        $q="UPDATE book SET bookava='$newbookava', bookrent='$newbookrent' where id='$bookid'";
+        if($this->connection->exec($q)){
+
+        $q="INSERT INTO issuebook (userid,issuename,issuebook,issuetype,issuedays,issuedate,issuereturn,fine)VALUES('$issueid','$userselect','$book','$issuetype','$days','$getdate','$returnDate','0')";
+
+        if($this->connection->exec($q)) {
+
+            $q="DELETE from requestbook where id='$redid'";
+            $this->connection->exec($q);
+            header("Location:admin_service_dashboard.php?msg=done");
+        }
+
+        else {
+            header("Location:admin_service_dashboard.php?msg=fail");
+        }
+        }
+        else{
+           header("Location:admin_service_dashboard.php?msg=fail");
+        }
+
+    }
+
+    else {
+        header("location: index.php?msg=Invalid Credentials");
+    }
+
+
+}
+
+function userdetail($id){
+    $q="SELECT * FROM userdata where id ='$id'";
+    $data=$this->connection->query($q);
+    return $data;
+}
+
+function userLogin($t1, $t2){
+    $q="SELECT * FROM userdata where email='$t1' and pass='$t2'";
+    $recordSet=$this->connection->query($q);
+    $result=$recordSet->rowCount();
+    if ($result > 0) {
+
+        foreach($recordSet->fetchAll() as $row) {
+            $logid=$row['id'];
+            $_SESSION["userid"]= $logid;
+            header("location: otheruser_dashboard.php?userlogid=$logid");
+        }
+    }
+
+    else {
+        header("location: index.php?msg=Invalid Credentials");
+    }
+
+}
+
+function requestbook($userid,$bookid){
+
+    $q="SELECT * FROM book where id='$bookid'";
+    $recordSetss=$this->connection->query($q);
+
+    $q="SELECT * FROM userdata where id='$userid'";
+    $recordSet=$this->connection->query($q);
+
+    foreach($recordSet->fetchAll() as $row) {
+        $username=$row['name'];
+        $usertype=$row['type'];
+    }
+
+    foreach($recordSetss->fetchAll() as $row) {
+        $bookname=$row['bookname'];
+    }
+
+    if($usertype=="student"){
+        $days=7;
+    }
+    if($usertype=="teacher"){
+        $days=21;
+    }
+
+
+    $q="INSERT INTO requestbook (id,userid,bookid,username,usertype,bookname,issuedays)VALUES('','$userid', '$bookid', '$username', '$usertype', '$bookname', '$days')";
+
+    if($this->connection->exec($q)) {
+        header("Location:otheruser_dashboard.php?userlogid=$userid");
+    }
+
+    else {
+        header("Location:otheruser_dashboard.php?msg=Connection failed!");
+    }
+
+}
+
+function requestbookdata(){
+    $q="SELECT * FROM requestbook ";
     $data=$this->connection->query($q);
     return $data;
 }
@@ -120,6 +338,12 @@ function deleteuserdata($id){
         header("Location:admin_service_dashboard.php?msg=Delete Failed!");
     }
 
+}
+
+function issuereport(){
+    $q="SELECT * FROM issuebook ";
+    $data=$this->connection->query($q);
+    return $data;
 }
 
 function issuebook($book,$userselect,$days,$getdate,$returnDate){
